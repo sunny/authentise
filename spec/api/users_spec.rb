6b4076c8ec0fa99e757564daf78c927a1b8be804
Sunny Ripert <sunny@sunfox.org>
2015-05-25 22:45:13 +0200

    Specs for errors in API

--- spec/api/users_spec.rb
+++ spec/api/users_spec.rb
@@ -3,27 +3,29 @@ require "authentise/api/users"
 
 describe Authentise::API::Users do
   describe "create_user" do
-    it "returns url and uuid" do
-      request_body = {
+    before do
+      @request_body = {
         email: "test@example.com",
         name: "Test User",
         username: "testuser",
         password: "password",
       }
-      request_headers = {
+      @request_headers = {
         "Accept" => "application/json",
         "Content-Type" => "application/x-www-form-urlencoded",
       }
-      response_body = {
+      @response_body = {
         name: "Test User",
         username: "testuser",
         uri: "https://example.com/user/1111",
         uuid: "1111",
       }.to_json
+    end
 
+    it "returns url and uuid" do
       stub_request(:post, "https://users.authentise.com/users/")
-        .with(body: request_body, headers: request_headers)
-        .to_return(status: 201, body: response_body)
+        .with(body: @request_body, headers: @request_headers)
+        .to_return(status: 201, body: @response_body)
 
       returned = Authentise::API::Users.create_user(
         email: "test@example.com",
@@ -38,31 +40,48 @@ describe Authentise::API::Users do
     end
 
     it "raises errors" do
-      skip
+      response_error_body = {
+        message: "Some test error"
+      }.to_json
+
+      stub_request(:post, "https://users.authentise.com/users/")
+        .with(body: @request_body, headers: @request_headers)
+        .to_return(status: 400, body: response_error_body)
+
+      assert_raises Authentise::API::Error do
+        Authentise::API::Users.create_user(
+          email: "test@example.com",
+          name: "Test User",
+          username: "testuser",
+          password: "password"
+        )
+      end
     end
 
   end
 
   describe "create_session" do
-    it "returns a token" do
-      request_body = {
+    before do
+      @request_body = {
         username: "testuser",
         password: "password",
       }
-      request_headers = {
+      @request_headers = {
         "Accept" => "application/json",
         "Content-Type" => "application/x-www-form-urlencoded"
       }
-      response_headers = {
+      @response_headers = {
         "Set-Cookie" => "session=f4242aef; " + \
                         "expires=Thu, 27-Apr-2017 08:49:20 GMT; " + \
                         "path=/; " + \
                         "domain=authentise.com",
       }
+    end
 
+    it "returns a token" do
       stub_request(:post, "https://users.authentise.com/sessions/")
-        .with(body: request_body, headers: request_headers)
-        .to_return(status: 201, body: "", headers: response_headers)
+        .with(body: @request_body, headers: @request_headers)
+        .to_return(status: 201, body: "", headers: @response_headers)
 
       returned = Authentise::API::Users.create_session(
         username: "testuser",
@@ -74,7 +93,20 @@ describe Authentise::API::Users do
     end
 
     it "raises errors" do
-      skip
+      response_error_body = {
+        message: "Some test error"
+      }.to_json
+
+      stub_request(:post, "https://users.authentise.com/sessions/")
+        .with(body: @request_body, headers: @request_headers)
+        .to_return(status: 400, body: response_error_body)
+
+      assert_raises Authentise::API::Error do
+        Authentise::API::Users.create_session(
+          username: "testuser",
+          password: "password"
+        )
+      end
     end
 
   end
